b27c9523d3bb8180e907e69a13e059392318f24e
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-07-08 05:27:23 -0700

    Let's make things difficult for myself by adopting a totally inappropriate namespace convention

--- remove_antiquated.py
+++
@@ -1,8 +0,0 @@
-'''
-This file meant to remove files with the old filename convention
-```
-
-from header import *
-
-paginator = s3.get_paginator('list_objects')
-# Direct S3 method (as opposed to database)
