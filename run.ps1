fe386ca647c488e70426578696b2db5b706a18bb
Selwyn-Lloyd <mypolopony@gmail.com>
2017-07-21 04:38:50 +0000

    Run.ps1

--- run.ps1
+++ run.ps1
@@ -7,7 +7,7 @@ cp E:\Projects\.config\rclone\rclone.conf ~\.config\
 
 cp E:\Projects\git\* ~\
 
-git pull -C E:\Projects\ScanOrchestrator
-git pull -C E:\Projects\MatlabCore
+git -C E:\Projects\ScanOrchestrator pull
+git -C E:\Projects\MatlabCore pull
 
 # python E:\Projects\ScanOrchestrator\orchestrator.py
