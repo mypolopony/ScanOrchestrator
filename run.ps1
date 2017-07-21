feaebe648b6014c3250d774be92eb9052914d478
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-07-20 23:41:38 -0700

    Debug

--- run.ps1
+++ run.ps1
@@ -1,13 +1,13 @@
 mkdir ~\.aws
-cp C:\AgriData\Projects\aws\credentials ~\.aws\
+cp C:\AgriData\Projects\aws\credentials ~\.aws\ > ~/help
 
-New-Item -ItemType Directory -Force -Path ~\.config
-New-Item -ItemType Directory -Force -Path ~\.config\rclone
-cp C:\AgriData\Projects\.config\rclone\rclone.conf ~\.config\
+New-Item -ItemType Directory -Force -Path ~\.config >> ~/help
+New-Item -ItemType Directory -Force -Path ~\.config\rclone >> ~/help
+cp C:\AgriData\Projects\.config\rclone\rclone.conf ~\.config\ >> ~/help
 
-cp C:\AgriData\Projects\git\* ~\
+cp C:\AgriData\Projects\git\* ~\ >> ~/help
 
-git -C C:\AgriData\Projects\ScanOrchestrator pull
-git -C C:\AgriData\Projects\MatlabCore pull
+git -C C:\AgriData\Projects\ScanOrchestrator pull >> ~/help
+git -C C:\AgriData\Projects\MatlabCore pull >> ~/help
 
-python C:\AgriData\Projects\ScanOrchestrator\orchestrator.py
+python C:\AgriData\Projects\ScanOrchestrator\orchestrator.py >> ~/help
