0ab43568b508211a1eedc33238b6440922d23135
Selwyn-Lloyd <mypolopony@gmail.com>
2017-07-21 11:58:02 +0000

    Run

--- run.ps1
+++ run.ps1
@@ -1,13 +1,16 @@
-mkdir ~\.aws
-cp C:\AgriData\Projects\aws\credentials ~\.aws\ > ~/help
+echo "hi" > C:\doinggood
+whoami >> C:\doinggood
 
-New-Item -ItemType Directory -Force -Path ~\.config >> ~/help
-New-Item -ItemType Directory -Force -Path ~\.config\rclone >> ~/help
-cp C:\AgriData\Projects\.config\rclone\rclone.conf ~\.config\ >> ~/help
+New-Item -ItemType Directory -Force -Path ~\.aws
+cp C:\AgriData\Projects\aws\credentials ~\.aws\
 
-cp C:\AgriData\Projects\git\* ~\ >> ~/help
+New-Item -ItemType Directory -Force -Path ~\.config
+New-Item -ItemType Directory -Force -Path ~\.config\rclone
+cp C:\AgriData\Projects\.config\rclone\rclone.conf C:\AgriData\.config\rclone\
+
+cp C:\AgriData\Projects\git\* ~\
 
 git -C C:\AgriData\Projects\ScanOrchestrator pull >> ~/help
 git -C C:\AgriData\Projects\MatlabCore pull >> ~/help
 
-python C:\AgriData\Projects\ScanOrchestrator\orchestrator.py >> ~/help
+# python C:\AgriData\Projects\ScanOrchestrator\orchestrator.py >> ~/help
