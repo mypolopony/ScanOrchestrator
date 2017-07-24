e27dcf9f76a6f46f5c46d43ce368766eaa697bb7
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-07-24 05:48:26 -0700

    Toss logging

--- run.ps1
+++ run.ps1
@@ -1,16 +1,16 @@
-New-Item -ItemType Directory -Force -Path C:\Users\agridata\.aws > C:\Users\agridata\status.log
-cp C:\AgriData\Projects\aws\credentialsC:\Users\agridata\.aws\ >> C:\Users\agridata\status.log
+New-Item -ItemType Directory -Force -Path C:\Users\agridata\.aws
+cp C:\AgriData\Projects\aws\credentialsC:\Users\agridata\.aws\ 
 
-New-Item -ItemType Directory -Force -Path C:\Users\agridata\.config >> C:\Users\agridata\status.log
-New-Item -ItemType Directory -Force -Path C:\Users\agridata\.config\rclone >> C:\Users\agridata\status.log
-cp C:\AgriData\Projects\.config\rclone\rclone.conf C:\Users\agridata\.config\rclone\ >> C:\Users\agridata\status.log
+New-Item -ItemType Directory -Force -Path C:\Users\agridata\.config 
+New-Item -ItemType Directory -Force -Path C:\Users\agridata\.config\rclone 
+cp C:\AgriData\Projects\.config\rclone\rclone.conf C:\Users\agridata\.config\rclone\ 
 
-cp C:\AgriData\Projects\git\* C:\Users\agridata\ >> C:\Users\agridata\status.log
+cp C:\AgriData\Projects\git\* C:\Users\agridata\ 
 
-git -C C:\AgriData\Projects\ScanOrchestrator pull >> C:\Users\agridata\status.log
-git -C C:\AgriData\Projects\MatlabCore pull >> C:\Users\agridata\status.log
+git -C C:\AgriData\Projects\ScanOrchestrator pull 
+git -C C:\AgriData\Projects\MatlabCore pull 
 
-pythonw C:\AgriData\Projects\ScanOrchestrator\orchestrator.py  >> C:\Users\agridata\status.log 
-echo "Done!" >> C:\Users\agridata\status.log
+pythonw C:\AgriData\Projects\ScanOrchestrator\orchestrator.py  
+echo "Done!" 
 
 exit
