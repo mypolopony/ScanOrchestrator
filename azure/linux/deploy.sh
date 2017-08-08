6045daab6a577a5ebee35bb2e7aa55f6ee54a66e
Koshy George <kgeorge2@gmail.com>
2017-08-07 21:41:56 -0700

    location specific paameter json files

--- azure/linux/deploy.sh
+++ azure/linux/deploy.sh
@@ -2,10 +2,10 @@
 
 
 
-DELETE=1
+DELETE=0
 CREATE=1
-LOC=westus2
-while getopts d:c:p: opts; do
+LOC=eastus
+while getopts d:c:l: opts; do
    case ${opts} in
       d) DELETE=${OPTARG} ;;
       c) CREATE=${OPTARG} ;;
@@ -14,11 +14,11 @@ while getopts d:c:p: opts; do
 done
 
 
-echo DELETE=$DELETE  CREATE=$CREATE
+echo DELETE=$DELETE  CREATE=$CREATE LOC=$LOC
 
 
 
-RG="detectionw2"
+RG="AEdetection"
 OUTDIR=./output
 DEPLOYMENT_NAME=$RG-depl
 mkdir -p $OUTDIR
@@ -29,19 +29,17 @@ if [[ -z KEPLONG ]]; then
 fi
 
 if [ "$CREATE" -eq "1" ];then
-    paramsJson=$(cat  $SRC_DIR/vmssdeploy.parameters.json | jq '.parameters')
+    paramsJson=$(cat  $SRC_DIR/vmssdeploy_$LOC.parameters.json | jq '.parameters')
     echo creating resource group
     az group create -n $RG   -l $LOC > $OUTDIR/$RG-output.json
     echo creating storage account $STORAGE
 
     templateFile=$SRC_DIR/vmssdeploy.json
-    paramsJson=$(cat  $SRC_DIR/vmssdeploy.parameters.json | jq '.parameters')
+    paramsJson=$(cat  $SRC_DIR/vmssdeploy_$LOC.parameters.json | jq '.parameters')
     echo $paramsJson
 
     #az group deployment validate -g $RG   --template-file $templateFile --parameters "$paramsJson" --verbose
-
-    az group deployment create -g $RG  -n $DEPLOYMENT_NAME  --template-file $templateFile --parameters "$paramsJson" --verbose >  $OUTDIR/$DEPLOYMENT_NAME-output.json
-
+     az group deployment create -g $RG  -n $DEPLOYMENT_NAME  --template-file $templateFile --parameters "$paramsJson" --verbose >  $OUTDIR/$DEPLOYMENT_NAME-output.json
 
 fi
 
@@ -50,7 +48,9 @@ fi
 
 if [ "$DELETE" -eq "1" ];then
     echo deleting resources
+    echo az group deployment delete -g $RG  -n $DEPLOYMENT_NAME
     az group deployment delete -g $RG  -n $DEPLOYMENT_NAME
+    echo az group delete -n $RG
     az group delete -n $RG
 fi
 
