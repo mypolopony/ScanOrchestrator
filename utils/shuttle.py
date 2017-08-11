83ccd548206dc8f43150eccd5ab0a09847fc104c
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-08-11 12:59:35 -0700

    Shuttle to save messages from queues

---
+++ utils/shuttle.py
@@ -0,0 +1,40 @@
+import pickle
+import os
+from kombu import Connection
+from datetime import datetime
+
+def receivefromRabbitMQ(queue, num=1):
+    msgs = list()
+    with Connection('amqp://{}:{}@{}:5672//'.format('agridata','agridata','boringmachine')) as kbu:
+        q = kbu.SimpleQueue(queue)
+        while (len(msgs) < num and q.qsize() > 0):
+            message = q.get(block=True)
+            message.ack()
+            msgs.append(message.payload)
+        q.close()
+
+        # If only one task is requested, release it from the list
+        if num == 1:
+            return msgs[0]
+        # Otherwise, return a list of tasks
+        else:
+            return msgs
+
+
+def sendtoRabbitMQ(queue, message):
+    with Connection('amqp://{}:{}@{}:5672//'.format('agridata','agridata','boringmachine')) as kbu:
+        q = kbu.SimpleQueue(queue)
+        q.put(message)
+        q.close()
+
+
+outdir = '/Users/mypolopony/AgriData/picklejar'
+target = 'dlq'
+tag = datetime.strftime(datetime.now(), '%D-%T').replace('/','').replace(':','')
+max_num = 999
+
+sendtoRabbitMQ(target, {'hello':'goodbye'})
+
+messages = receivefromRabbitMQ(target, max_num)
+with open(os.path.join(outdir, target + '-' + tag + '.dat'), 'wb') as dump:
+    pickle.dump(messages, dump)
