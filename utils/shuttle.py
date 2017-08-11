455d89c105af65036c8e3a8d5a94a1e6f0843137
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-08-11 14:25:45 -0700

    Finish shuttle

--- utils/shuttle.py
+++ utils/shuttle.py
@@ -2,24 +2,19 @@ import pickle
 import os
 from kombu import Connection
 from datetime import datetime
+import time
 
-def receivefromRabbitMQ(queue, num=1):
+def receivefromRabbitMQ(queue):
     msgs = list()
     with Connection('amqp://{}:{}@{}:5672//'.format('agridata','agridata','boringmachine')) as kbu:
         q = kbu.SimpleQueue(queue)
-        while (len(msgs) < num and q.qsize() > 0):
-            message = q.get(block=True)
+        print(q.qsize())
+        for i in xrange(q.qsize()):
+            message = q.get()
             message.ack()
             msgs.append(message.payload)
         q.close()
-
-        # If only one task is requested, release it from the list
-        if num == 1:
-            return msgs[0]
-        # Otherwise, return a list of tasks
-        else:
-            return msgs
-
+    return msgs
 
 def sendtoRabbitMQ(queue, message):
     with Connection('amqp://{}:{}@{}:5672//'.format('agridata','agridata','boringmachine')) as kbu:
@@ -34,7 +29,7 @@ replace = True
 tag = datetime.strftime(datetime.now(), '%D-%T').replace('/','').replace(':','')
 max_num = 999
 
-messages = receivefromRabbitMQ(target, max_num)
+messages = receivefromRabbitMQ(target)
 with open(os.path.join(outdir, target + '-' + tag + '.dat'), 'wb') as dump:
     pickle.dump(messages, dump)
 
