5fd6690e3bf7cfb6b5c67af161fc51874069694f
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-08-12 10:23:09 -0700

    Proper reporting for process

--- orchestrator.py
+++ orchestrator.py
@@ -738,7 +738,7 @@ def process(args):
             m['role'] = 'process'
 
         # Notify
-        log('Received detection tasks: {}'.format([m['detection_params']['result'] for m in multi_task]), multi_task[0]['session_name'])
+        log('Received processing tasks: {}'.format([m['detection_params']['result'] for m in multi_task]), multi_task[0]['session_name'])
 
         try:
             # Rebuild base scan info (just use the first task)
