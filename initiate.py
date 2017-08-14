ea2ea7494f2029431b1417f93f25f6e8dd9855b2
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-08-14 15:51:30 -0700

    initial commit, beginning work on initiate

--- initiate.py
+++ initiate.py
@@ -25,6 +25,13 @@ config_path = os.path.join(config_parent_dir, 'utils', 'poller.conf')
 assert (os.path.isfile(config_path))
 config.read(config_path)
 
+# ANSI escape (to remove color codes from subprocess output)
+ansi_escape = re.compile(r'\x1b[^m]*m')
+
+# Azure connection (keys = ['tenant', 'tokenType', 'expiresOn', 'accessToken', 'subscription'])
+auth = subprocess.check_output(['az','account','generate-access-id'])
+auth = json.loads(ansi_escape.sub('', auth))
+
 def sendtoKombu(queue, task):
     with Connection('amqp://{}:{}@{}:5672//'.format(config.get('rmq', 'username'),
                                                     config.get('rmq', 'password'),
@@ -33,10 +40,9 @@ def sendtoKombu(queue, task):
         q.put(task)
         q.close()
 
-
 def parse_args():
-    parser = argparse.ArgumentParser('initiate')
-    parser.add_argument('-s', '--session_name', help='session_name', dest='session_name', required=True)
+    parser=argparse.ArgumentParser('initiate')
+    parser.add_argument('-s', '--session_name', help='session_name', dest='session_name',required=True)
     args = parser.parse_args()
     return args
 
