a82feaf7cea0bc16fb925c87e541f6edd58fc454
Selwyn-Lloyd McPherson <mypolopony@gmail.com>
2017-08-15 14:33:50 -0700

    Corona South

--- orchestrator.py
+++ orchestrator.py
@@ -834,7 +834,7 @@ def getComputerInfoString():
 def parse_args():
     parser=argparse.ArgumentParser('orchestrator')
     default_role='Unknown'
-    default_session='coronanorth'
+    default_session='coronasouth'
     default_service_namespace = 'agridataqueues2'
     default_shared_access_key_name = 'sharedaccess'
     default_shared_access_key_value = 'eEoOu6rVzuUCAzKJgW5OqzwdVoqiuc2xxl3UEieUQLA='
