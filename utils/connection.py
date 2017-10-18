5b5c5e6f9e84581d2e9a1e3e15ccb73ebb94baaa
Koshy George <kgeorge2@gmail.com>
2017-10-18 13:58:00 -0700

    new mlab settings with replicated servers

--- utils/connection.py
+++ utils/connection.py
@@ -1,24 +1,35 @@
+from models_min import *
 from mongoengine import connect
 from pymongo import MongoClient
-from bson.objectid import ObjectId
-import os
-import boto3
 
-# MongoDB
+db = 'local'
 
-print('Connecting to Development Databae')
-dbname = 'dev-agdb'
-username = 'staging-agridata'
-password = 'M5m7xMe2cUNcNn6y$Rz'
-server = 'ds036947-a0.mlab.com'
-port = 36947
-connect(dbname, host=server, port=port, username=username, password=password)
-c = MongoClient('mongodb://' + username + ':' + password + '@' + server + ':' + str(port) + '/' + dbname)
-db = c[dbname]
+if db == 'local':
+    
+    print('Connecting to Local DB')
+    dbname = 'dev-agdb'
+    connect(dbname)
+    c = MongoClient('mongodb://localhost:27017/' + dbname)
+    db=c[dbname]
 
-'''
-dbname = 'dev-agdb'
-connect(dbname)
-c = MongoClient('mongodb://localhost:27017')
-db = c[dbname]
-'''
+else:
+
+    if db == 'staging':
+        print('Connecting to Development DB')
+        dbname = 'dev-agdb'
+        username = 'staging-agridata'
+        password = 'M5m7xMe2cUNcNn6y$Rz'
+        server = ['ds036947-a0.mlab.com:36947', 'ds036947-a1.mlab.com:36942']
+        port = 36947
+
+    if db == 'production':
+        print('Connecting to Production DB')
+        dbname = 'agdb'
+        username = 'production-agridata'
+        password = '8svcjPZYtx$7F+%p?sBv'
+        server = ['ds036937-a0.mlab.com:36937','ds036937-a1.mlab.com:36932']
+        port = 36937
+
+    connect(dbname, host=server, port=port, username=username, password=password)
+    c = MongoClient('mongodb://' + username + ':' + password + '@' + ','.join(server) + '/' + dbname)
+    db = c[dbname]
